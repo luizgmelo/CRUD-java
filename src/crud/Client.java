96eddbb8bfcfeb485ea2e36da913d34f05b7e58a
Luiz Guilherme <88911920+luizgmelo@users.noreply.github.com>
2024-03-23 19:06:34 -0300

    feat: add crud operations create, read, update and delete

--- src/crud/Client.java
+++ src/crud/Client.java
@@ -18,4 +18,41 @@ public class Client implements Crud {
 		return this.name;
 	}
 	
+	@Override
+	public void writePost(String post) {
+		this.posts.add(post);
+		System.out.println("POST CREATED NUMBER: " + this.numPosts);
+		this.numPosts++;
+	}
+
+	@Override
+	public void readPost(int postNumber) {
+		if (postNumber < 0 || postNumber >= posts.size()) {
+			System.out.println("INVALID POST NUMBER");
+		} else {
+			System.out.println(posts.get(postNumber));
+		}
+	}
+
+	@Override
+	public void updatePost(int postNumber, String newPost) {
+		if (postNumber < 0 || postNumber >= posts.size()) {
+			System.out.println("INVALID POST NUMBER");
+		} else {
+			this.posts.set(postNumber, newPost);
+			System.out.println("POST NUMBER" + postNumber + " MODIFIED");	
+		}
+	}
+
+	@Override
+	public void deletePost(int postNumber) {
+		if (postNumber < 0 || postNumber >= posts.size()) {
+			System.out.println("INVALID POST NUMBER");
+		} else {
+			this.posts.remove(postNumber);
+			System.out.println("POST NUMBER " + postNumber + " REMOVED");
+		}
+	}
+	
+	
 }
